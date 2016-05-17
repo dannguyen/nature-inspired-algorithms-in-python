257f19a15537805524f9991752b7963b492ecc0c
Dan Nguyen <dansonguyen@gmail.com>
2016-05-17 07:32:48 -0700

    Update adaptive-random-search.py

--- adaptive-random-search.py
+++ adaptive-random-search.py
@@ -3,7 +3,13 @@ from random import randrange
 def objective_function(vector):
     return sum(x ** 2 for x in vector)
     
-def rand_in_bounds(min, max):
-    return randrange(min, max)
-  
 def random_vector(minmax):
+    return [randrange(minmax[i][0], minmax[i][1]) for i in range(len(minmax))]
+    
+def take_step(minmax, current, step_size):
+    arr = []
+    for i in len(current):
+        _min = max([minmax[i][0], current[i] - step_size])
+        _max = min([minmax[i][1], current[i] + step_size])
+        arr[i] = randrange(_min, _max)
+    return arr
